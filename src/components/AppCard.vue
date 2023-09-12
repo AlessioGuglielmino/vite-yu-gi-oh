94b9fe2d7653edd78e7100dc35727c00a960d34f
AlessioGugielmino <guglielmino.alessio@virgilio.it>
2023-09-12 15:40:56 +0200

    Add-Components

---
+++ src/components/AppCard.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template lang="scss" scoped></template>
+
+<style></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "hello",
+    };
+  },
+};
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
